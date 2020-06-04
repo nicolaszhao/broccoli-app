dfbb452673183a667165fa14906e0330b14168d2
ZhaoXiaodong <zhaoxd@wifi.com>
2020-06-04 17:50:33 +0800

    feat: 搞定基本功能

--- src/App.js
+++ src/App.js
@@ -1,50 +1,142 @@
-import React, { useEffect } from 'react';
-import {
-  BrowserRouter as Router, Route, NavLink, Switch,
-} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { hot } from 'react-hot-loader/root';
-import Home from './pages/Home';
-import NoMatch from './pages/NoMatch';
+import { Modal } from '@totebox/ui';
+import InviteForm from './components/InviteForm';
+import * as api from './api';
 import './app.scss';
 
-const About = React.lazy(() => import('./pages/About'));
+const useInvite = (formData) => {
+  const [state, setState] = useState({
+    fetching: false,
+    error: null,
+    ok: false,
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+
+    const invite = async ({ name, email }) => {
+      setState(() => ({ fetching: true, error: null, ok: false }));
+      try {
+        await api.invite({ name, email });
+        !didCancel && setState((prevState) => ({ ...prevState, fetching: false, ok: true }));
+      } catch (error) {
+        !didCancel && setState(
+          (prevState) => ({ ...prevState, fetching: false, error: new Error(error) }),
+        );
+      }
+    };
+
+    const reset = () => setState(() => ({ fetching: false, error: null, ok: false }));
+
+    if (!formData) {
+      reset();
+    } else {
+      invite(formData);
+    }
+
+    return () => {
+      didCancel = true;
+    };
+  }, [formData]);
+
+  return state;
+};
 
 const App = () => {
+  const [formData, setFormData] = useState(null);
+  const [inviteModalVisible, setInviteModalVisible] = useState(false);
+  const [okModalVisible, setOkModalVisible] = useState(false);
+  const { fetching, ok, error } = useInvite(formData);
+
+  function handleInviteFormSend(data) {
+    setFormData(data);
+  }
+
+  function handleInviteModalClose() {
+    setFormData(null);
+    setInviteModalVisible(false);
+  }
+
   useEffect(() => {
-    // for element:active
-    document.body.addEventListener('touchstart', () => {});
+    document.addEventListener('touchstart', () => {});
   }, []);
 
+  useEffect(() => {
+    if (ok) {
+      setInviteModalVisible(false);
+      setOkModalVisible(true);
+    }
+  }, [ok]);
+
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="container">
-        <h1>React SPA Boilerplate</h1>
-        <nav className="nav">
-          <ul>
-            <li>
-              <NavLink to="/" activeClassName="active" exact>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" activeClassName="active">
-                About
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div className="content">
-          <React.Suspense fallback={<div className="loading">Loading...</div>}>
-            <Switch>
-              <Route path="/" component={Home} exact />
-              <Route path="/about" component={About} />
-              <Route component={NoMatch} />
-            </Switch>
-          </React.Suspense>
+    <main className="container">
+      <header className="header">
+        <h1>Broccoli &amp; Co.</h1>
+      </header>
+      <section className="content">
+        <article className="main-content">
+          <h2 className="slogan">
+            A better way to
+            <br />
+            to enjoy every day.
+          </h2>
+          <p className="sub-slogan">Be the first to know when we launch.</p>
+          <p>
+            <button
+              className="button invite-button"
+              type="button"
+              onClick={() => setInviteModalVisible(true)}
+            >
+              Request an invite
+            </button>
+          </p>
+        </article>
+      </section>
+      <footer className="footer">
+        <p className="footer-desc">
+          Made with
+          <span className="footer-love">❤</span>
+          in Melbourne.
+        </p>
+        <p className="footer-license">
+          &copy;
+          <span className="footer-copy">{new Date().getFullYear()}</span>
+          Broccoli &amp; Co. All rights reserved.
+        </p>
+      </footer>
+      <Modal
+        title="Request an invite"
+        visible={inviteModalVisible}
+        animation="flip"
+        onClose={handleInviteModalClose}
+      >
+        <div className="invite-modal-wrapper">
+          <InviteForm
+            sending={fetching}
+            onSend={handleInviteFormSend}
+          />
+          {error && (
+            <p className="invite-error">
+              {error.message}
+            </p>
+          )}
         </div>
-      </div>
-    </Router>
+      </Modal>
+      <Modal
+        title="All done!"
+        visible={okModalVisible}
+        animation="flip"
+        onClose={() => setOkModalVisible(false)}
+      >
+        <div className="invite-modal-wrapper">
+          <p className="invite-ok">
+            You will be one of the first to experience Broccoli &amp; Co. when we launch.
+          </p>
+        </div>
+      </Modal>
+    </main>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './configs/public-path';
-import './assets/base.scss';
+import './publicPath';
+import './index.scss';
+import './assets/font.scss';
+import '@totebox/ui/dist/ui.css';
 
 import React from 'react';
 import { render } from 'react-dom';
