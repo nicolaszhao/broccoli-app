d5ec31e9177499014c65267d9b0459c1e007e0ef
ZhaoXiaodong <zhaoxd@wifi.com>
2020-06-04 17:49:17 +0800

    feat: 清理脚手架之前的文件

--- src/configs/public-path.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-__webpack_public_path__ = process.env.PUBLIC_URL;
--- src/pages/NoMatch/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NoMatch = () => (
-  <h2>404</h2>
-);
-
-export default NoMatch;
